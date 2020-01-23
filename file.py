b6983afcfd324547ebb597bbecf963263a647291
Agathe de Drouas <agathededrouas@Agathes-MacBook-Air.local>
2020-01-23 14:44:23 +0100

    add file.py to git

---
+++ file.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+2*3
+
+#%%
+# Creating integers
+
+4
+
+
+3.14
+
+
+11//5
+
+3.14 * 2 ** 2
+
+#%%
+
+(3.14*4/3)*(2**3)
+
+#"na" * "wer"
+
+#%%
+
+age = input ("tell me your age : ")
+
+print (2020 - int(age))
+
+#%% Exercises
+
+#1
+
+60*60
+
+#3
+
+age = input ("tell me your age ; ")
+
+mom_age = input ("tell me your nothers age :")
+
+print(int(mom_age) - int(age))
+
+#%%
+
+#4
+
+3//11
+
+25%2
+
+#5
+
+'there are ' + str(4) +' dogs barking'
+
+
+
